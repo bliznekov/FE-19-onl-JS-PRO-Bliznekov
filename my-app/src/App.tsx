87b7c1525244e651de013695d173ec99c9070034
bliznekov <r.bliznekov@gmail.com>
2022-04-03 16:56:10 +0300

    Add hometask-4

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,13 +3,17 @@ import './App.scss';
 import Header from './component/header/Header';
 import Main from './component/main/Main';
 import Posts from './component/posts/Posts';
+import Timer from './component/timer/Timer';
 
-function App() {
+
+
+const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
       <Main />
       <Posts />
+      <Timer />
     </div>
   );
 }
