5a7ca8ade88ce498f23dcee97ae0c1b00ee8bec1
bliznekov <r.bliznekov@gmail.com>
2022-05-15 13:04:56 +0300

    Added authSlice and createTokens thunk

--- my-app/src/component/login/Login.tsx
+++ my-app/src/component/login/Login.tsx
@@ -1,56 +1,52 @@
 import React, { useState } from "react";
 import FormValuesType from "../../types/formValuesType";
+import { useActions } from "../hooks/useActions";
+import { useSelector } from "../hooks/useSelector";
 import useTranslate from "../hooks/useTranslate";
 import Button from "../ui/button/Button";
 import FormCard from "../ui/formCard/FormCard";
 import FormTextField from "../ui/formTextField/FormTextField";
 
-
 const Login: React.FC = () => {
     const [values, setValues] = useState<FormValuesType>({});
     const { t } = useTranslate();
+    const { createTokens } = useActions();
+    const loading = useSelector((state) => state.auth.loading);
+    const error = useSelector((state) => state.auth.error);
 
     const handleSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
 
-        console.log(values);
-    }
+        createTokens(values);
+    };
 
     return (
-        <FormCard header="Login">
-            <FormTextField
-                autofocus
-                label={t("login.name")}
-                name="name"
-                values={values}
-                setValues={setValues}
-            />
-            <FormTextField
-                label={t("login.email")}
-                type="email"
-                name="email"
-                values={values}
-                setValues={setValues}
-            />
-            <FormTextField
-                label={t("login.password")}
-                type="password"
-                name="password"
-                values={values}
-                setValues={setValues}
-            />
-            <FormTextField
-                label={t("login.confirmPassword")}
-                type="password"
-                name="confirmPassword"
-                values={values}
-                setValues={setValues}
-            />
-            <Button onClick={handleSubmit}>
-                {t("login.submit")}
-            </Button>
-        </FormCard>
-    )
-}
+        <div className="center__content">
+            <FormCard header="Login" loading={loading}>
+                <FormTextField
+                    autofocus
+                    label="Email"
+                    type="email"
+                    name="email"
+                    values={values}
+                    setValues={setValues}
+                />
+                <FormTextField
+                    label="Password"
+                    type="password"
+                    name="password"
+                    values={values}
+                    setValues={setValues}
+                />
+                {error && (
+                    <div className="form-error">
+                        No active account found with the given credentials
+                    </div>
+                )}
+                <Button onClick={handleSubmit}>{t("login.submit")}</Button>
+            </FormCard>
+        </div>
+    );
+};
 
 export default Login;
--- my-app/src/component/ui/formCard/FormCard.tsx
+++ my-app/src/component/ui/formCard/FormCard.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import { CircularProgress } from "@mui/material";
+import React from "react";
 
 import "./FromCard.scss";
 
 type PropsType = {
-    header?: string
-}
+    header?: string;
+    loading?: boolean;
+};
 
-const FormCard: React.FC<PropsType> = ({ header, children }) => {
-
+const FormCard: React.FC<PropsType> = ({ header, loading, children }) => {
     return (
-        <form className='form-card-container'>
-            {header &&
-                <div className='header'>{header}</div>
-            }
-            {children}
+        <form className={`form-card-container`}>
+            <div className={`form-content ${loading ? "_loading" : ""}`}>
+                {header && <div className="header">{header}</div>}
+                {children}
+            </div>
+            {loading && (
+                <div className="loader">
+                    <CircularProgress />
+                </div>
+            )}
         </form>
-    )
-}
+    );
+};
 
 export default FormCard;
